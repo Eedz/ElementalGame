01a4cebe5368e1aef9d996692deb6a8bc5996c56
eddiebauer0 <eddiebauer0@gmail.com>
2019-02-18 22:59:32 -0500

    Add project files.

---
+++ ElementalGame/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+namespace ElementalGame
+{
+    public partial class Form1 : Form
+    {
+
+        List<Rectangle> BoardSpaces;
+        int SpaceHeight = 10;
+        int SpaceWidth = 10;
+        public Form1()
+        {
+            InitializeComponent();
+
+            BoardSpaces.Add(new Rectangle(this.Width / 2 - SpaceWidth, this.Height / 2 + SpaceHeight, SpaceWidth, SpaceHeight));
+            BoardSpaces.Add(new Rectangle(this.Width / 2 - SpaceWidth, this.Height / 2 + SpaceHeight, SpaceWidth, SpaceHeight));
+        }
+
+
+
+        private void DrawRectangle(Rectangle r)
+        {
+            System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+            System.Drawing.Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            List<Point> points;
+            points.Add(new Point(this.Width / 2 - SpaceWidth, this.Height / 2 + SpaceHeight))
+            
+            formGraphics.DrawPolygon(myPen, points);
+            myPen.Dispose();
+            formGraphics.Dispose();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(Rectangle r in BoardSpaces)
+                DrawRectangle(r);
+        }
+    }
+}
---
+++ Engine/Class1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+
+namespace Engine
+{
+    public enum MarbleState { Frozen, Free }
+    public enum MarbleType { Salt, Earth, Fire, Water, Air, Vitae, Mors, Quicksilver, Lead, Tin, Iron, Copper, Silver, Gold }
+
+    public class Marble
+    {
+        public MarbleState State { get; set; }
+        public MarbleType Element { get; set; }
+        public List<MarbleType> Matchers { get; set; }
+
+        public Marble(MarbleState state, MarbleType element)
+        {
+            State = state;
+            Element = element;
+            GetMatchers();
+        }
+
+        public void GetMatchers()
+        {
+            switch (Element)
+            {
+                case MarbleType.Air:
+                    Matchers.Add(MarbleType.Air);
+                    Matchers.Add(MarbleType.Salt);
+                    break;
+                case MarbleType.Water:
+                    Matchers.Add(MarbleType.Water);
+                    Matchers.Add(MarbleType.Salt);
+                    break;
+                case MarbleType.Earth:
+                    Matchers.Add(MarbleType.Earth);
+                    Matchers.Add(MarbleType.Salt);
+                    break;
+                case MarbleType.Fire:
+                    Matchers.Add(MarbleType.Fire);
+                    Matchers.Add(MarbleType.Salt);
+                    break;
+                case MarbleType.Salt:
+                    Matchers.Add(MarbleType.Air);
+                    Matchers.Add(MarbleType.Water);
+                    Matchers.Add(MarbleType.Earth);
+                    Matchers.Add(MarbleType.Fire);
+                    Matchers.Add(MarbleType.Salt);
+                    break;
+                case MarbleType.Vitae:
+                    Matchers.Add(MarbleType.Mors);
+                    break;
+                case MarbleType.Mors:
+                    Matchers.Add(MarbleType.Vitae);
+                    break;
+                case MarbleType.Quicksilver:
+                    Matchers.Add(MarbleType.Lead);
+                    Matchers.Add(MarbleType.Tin);
+                    Matchers.Add(MarbleType.Iron);
+                    Matchers.Add(MarbleType.Copper);
+                    Matchers.Add(MarbleType.Silver);
+                    Matchers.Add(MarbleType.Gold);
+                    break;
+                case MarbleType.Lead:
+                    Matchers.Add(MarbleType.Quicksilver);
+                    break;
+                case MarbleType.Tin:
+                    Matchers.Add(MarbleType.Quicksilver);
+                    break;
+                case MarbleType.Iron:
+                    Matchers.Add(MarbleType.Quicksilver);
+                    break;
+                case MarbleType.Copper:
+                    Matchers.Add(MarbleType.Quicksilver);
+                    break;
+                case MarbleType.Silver:
+                    Matchers.Add(MarbleType.Quicksilver);
+                    break;
+                case MarbleType.Gold:
+                    break;
+            }
+        }
+
+        public void Calcify()
+        {
+            Element = MarbleType.Salt;
+        }
+
+        public bool IsMatch(Marble m)
+        {
+            if (Matchers.Contains(m.Element))
+                return true;
+            else return false;
+        }
+
+        public void Ascend()
+        {
+            switch (Element)
+            {
+                case MarbleType.Lead:
+                    Element = MarbleType.Tin;
+                    break;
+                case MarbleType.Tin:
+                    Element = MarbleType.Iron;
+                    break;
+                case MarbleType.Iron:
+                    Element = MarbleType.Copper;
+                    break;
+                case MarbleType.Copper:
+                    Element = MarbleType.Silver;
+                    break;
+                case MarbleType.Silver:
+                    Element = MarbleType.Gold;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+
